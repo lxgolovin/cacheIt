87eef0a634768783fdaa5452e49e38f4306c9542
Alexander Golovin <lxgolovin@gmail.com>
2019-06-02 22:03:11 +0300

    Few tests and methods added to CacheController

--- src/main/java/com/lxgolovin/cache/CacheController.java
+++ src/main/java/com/lxgolovin/cache/CacheController.java
@@ -1,4 +1,44 @@
 package com.lxgolovin.cache;
 
-public class CacheController {
+import java.util.LinkedList;
+import java.util.List;
+
+public class CacheController<K,V> {
+
+    private LinkedList<Cache> cclist = new LinkedList<>();
+
+    public CacheController() {
+        CacheAlgorithm<K> lru = new LruMru<>();
+        Cache<K, V> cache = new MemoryCache<>(lru);
+        addLevel(cache);
+    }
+
+    public int addLevel(Cache<K,V> cache) {
+        cclist.add(cache);
+        return cclist.size();
+    }
+
+    public void removeLevel(int index) {
+        //TODO: possibly need to clean cache and move data to next levels
+        cclist.remove(index);
+    }
+
+    public int levels() {
+        return cclist.size();
+    }
+
+    /**
+     * @param key if null, returns false
+     * @param value if null, returns false
+     * @return returns true if success, else false
+     */
+    public boolean load(K key, V value) {
+        if ( key == null | value == null ) { return false; }
+        return true;
+    }
+
+    public List<K> getData() {
+        //TODO: export all data to List
+        return null;
+    }
 }
--- src/test/java/com/lxgolovin/cache/CacheControllerTest.java
+++ src/test/java/com/lxgolovin/cache/CacheControllerTest.java
@@ -1,7 +1,39 @@
 package com.lxgolovin.cache;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CacheControllerTest {
+    private CacheController<Integer, Integer> cc;
+    Cache<Integer, Integer> lruCache;
 
+    @BeforeEach
+    void setUp() {
+        cc = new CacheController<>();
+
+        CacheAlgorithm<Integer> lru = new LruMru<>();
+        lruCache = new MemoryCache<>(lru);
+    }
+
+    @Test
+    void addRemoveCacheLevels() {
+        assertEquals(2,cc.addLevel(lruCache));
+        cc.removeLevel(1);
+        assertEquals(1, cc.levels());
+
+    }
+
+    @Test
+    void numberOfLevels() {
+        assertEquals(1, cc.levels());
+    }
+
+    @Test
+    void loadData() {
+        assertTrue(cc.load(1,1));
+    }
 }
